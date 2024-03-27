10e7a50931a8a6be975bec0f9e148152b5ab4d2d
Sebastian <78531972+SebastianCaron@users.noreply.github.com>
2024-03-27 16:09:48 +0100

    TEST FTP

---
+++ Auto/ftp_deploiement.py
@@ -0,0 +1,29 @@
+import sys
+import ftplib
+import os
+
+
+FTP_HOST = 'ftp.cluster031.hosting.ovh.net'
+FTP_TARGET_DIR = '/www/projets/Olympics'
+
+def ftp_upload(local_dir, username, password):
+    try:
+        ftp = ftplib.FTP(FTP_HOST, username, password)
+        ftp.cwd(FTP_TARGET_DIR)
+        for file in os.listdir(local_dir):
+            if os.path.isfile(os.path.join(local_dir, file)):
+                with open(os.path.join(local_dir, file), 'rb') as f:
+                    ftp.storbinary('STOR ' + file, f)
+        ftp.quit()
+        print("Fichiers OK !")
+    except Exception as e:
+        print("Erreur : ", e)
+
+
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    ftp_upload(os.getcwd() + "/Website/", username, password)
+    
