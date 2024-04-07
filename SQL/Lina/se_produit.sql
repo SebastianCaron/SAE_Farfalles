d683bd6a3619b0fc7c81cbd51dd526a44a4193ef
Sebastian <78531972+SebastianCaron@users.noreply.github.com>
2024-04-07 21:09:15 +0200

    Ajout Se_Produit.sql

---
+++ SQL/Lina/se_produit.sql
@@ -0,0 +1,2 @@
+INSERT INTO Se_Produit (ID_Ceremonies, Longitude_Sites, Latitude_Sites, Date_Debut, Date_Fin) VALUES ('OUVERTURE',2.29221884,48.85972558,'2024-07-26','2024-08-11');
+INSERT INTO Se_Produit (ID_Ceremonies, Longitude_Sites, Latitude_Sites, Date_Debut, Date_Fin) VALUES ('CLOTURE',2.360127,48.924475,'2024-08-11','2024-08-11');
