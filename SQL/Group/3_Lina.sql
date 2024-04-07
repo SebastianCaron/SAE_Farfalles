8ae0c16918a89afe3dccbe52e73978b8139e4562
GitHub Action <action@github.com>
2024-04-07 19:09:53 +0000

    Add SQL generated file

--- SQL/Group/3_Lina.sql
+++ SQL/Group/3_Lina.sql
@@ -1,3 +1,7 @@
+/* se_produit.sql */
+INSERT INTO Se_Produit (ID_Ceremonies, Longitude_Sites, Latitude_Sites, Date_Debut, Date_Fin) VALUES ('OUVERTURE',2.29221884,48.85972558,'2024-07-26','2024-08-11');
+INSERT INTO Se_Produit (ID_Ceremonies, Longitude_Sites, Latitude_Sites, Date_Debut, Date_Fin) VALUES ('CLOTURE',2.360127,48.924475,'2024-08-11','2024-08-11');
+
 /* ceremonies.sql */
 INSERT INTO Ceremonies (ID_Ceremonies, Nom_Ceremonies) VALUES ("OUVERTURE","Cérémonie d'ouverture des JO 2024");
 INSERT INTO Ceremonies (ID_Ceremonies, Nom_Ceremonies) VALUES ("CLOTURE","Cérémonie de clôture des JO 2024");
