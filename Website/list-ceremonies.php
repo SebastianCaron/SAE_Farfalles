a6f83b9eceb32d31e062938cb933bafef7e871b2
Rururubis <127145390+Rururubis@users.noreply.github.com>
2024-04-08 18:31:40 +0200

    Update list-ceremonies.php

--- Website/list-ceremonies.php
+++ Website/list-ceremonies.php
@@ -57,7 +57,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Template</title>
+	<title>Liste Cérémonies</title>
 	<link rel="stylesheet" type="text/css" href="./css/all.css">
 
 	<!-- VOTRE CSS -->
