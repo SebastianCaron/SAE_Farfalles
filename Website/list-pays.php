01eb42ab47896d7a1f5bad618b0d6657c8764ac5
DuPeio <142165526+DuPeio@users.noreply.github.com>
2024-04-08 22:21:00 +0200

    Update list-pays.php

--- Website/list-pays.php
+++ Website/list-pays.php
@@ -102,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <?php foreach ($pays as $pays) : ?>
                 <li>
                     <img src="<?php echo $pays['Drapeau']; ?>" alt="Drapeau du pays">
-                    <?php echo $pays['ID']; ?>,
+                    <strong><?php echo $pays['ID']; ?></strong>
                     <span>Nom Français : </span> <?php echo $pays['Nom_Français']; ?>
                     <span>Nom Anglais : </span> <?php echo $pays['Nom_Anglais']; ?>
                 </li>
