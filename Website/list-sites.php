b73a4abad38cac2466b067b2c90f966b51d82429
Quentin_F <164506507+QuentinF04@users.noreply.github.com>
2024-04-08 22:08:13 +0200

    Add files via upload

--- Website/list-sites.php
+++ Website/list-sites.php
@@ -39,7 +39,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Paris 2024 - Sites</title>
-    <!-- <link rel="stylesheet" type="text/css" href="./css/all.css"> -->
+    <link rel="stylesheet" type="text/css" href="./css/all.css">
 
 
     <!-- VOTRE CSS -->
