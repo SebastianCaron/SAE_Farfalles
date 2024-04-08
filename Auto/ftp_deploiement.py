e4356ce13985509543612ca326bbdacafb019d38
Sebastian <78531972+SebastianCaron@users.noreply.github.com>
2024-04-08 14:53:21 +0200

    Changements Deploiement

--- Auto/ftp_deploiement.py
+++ Auto/ftp_deploiement.py
@@ -13,20 +13,26 @@ def ftp_upload(local_dir, username, password):
     try:
         ftp = ftplib.FTP(FTP_HOST, username, password)
         ftp.cwd(FTP_TARGET_DIR)
-        
-        for item in os.listdir(local_dir):
-            local_path = os.path.join(local_dir, item)
-            if os.path.isfile(local_path):
-                remote_path = os.path.join(FTP_TARGET_DIR, item)
-                if item != EXCEPTION_FILE.split('/')[-1]:
-                    if should_upload(local_path, remote_path, ftp):
-                        upload_file(ftp, local_path, remote_path)
-            elif os.path.isdir(local_path):
-                ftp_upload(local_path, username, password)
-        
+        upload_dir_contents(ftp, local_dir)
         ftp.quit()
     except ftplib.all_errors as e:
         print("FTP error:", e)
+
+def upload_dir_contents(ftp, local_dir):
+    for item in os.listdir(local_dir):
+        local_path = os.path.join(local_dir, item)
+        remote_path = os.path.join(FTP_TARGET_DIR, item)
+        if os.path.isfile(local_path):
+            if item != EXCEPTION_FILE.split('/')[-1]:
+                if should_upload(local_path, remote_path, ftp):
+                    upload_file(ftp, local_path, remote_path)
+        elif os.path.isdir(local_path):
+            try:
+                ftp.mkd(remote_path)
+            except ftplib.error_perm as e:
+                if "550" not in str(e):
+                    print("Error creating remote directory:", e)
+            upload_dir_contents(ftp, local_path)
 
 def should_upload(local_file, remote_file, ftp):
     try:
