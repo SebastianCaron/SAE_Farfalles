362ed10bffdfd44708b748f5d786ef4b3396f63e
Quentin_F <164506507+QuentinF04@users.noreply.github.com>
2024-04-08 23:42:16 +0200

    Update list-sites.php

--- Website/list-sites.php
+++ Website/list-sites.php
@@ -103,7 +103,8 @@
                 <th>Date de construction</th>
                 <th>Capacité d'accueil</th>
                 <th>Accessibilité</th>
-                <th id="epreuves_column" class="hide">Épreuves</th> 
+                <th id="epreuves_column" class="hide">Épreuves</th>
+                <br>
             </tr>
 
             <script>
