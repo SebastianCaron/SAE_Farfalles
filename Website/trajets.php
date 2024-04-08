9eac42ead3d05e417b0e036b8945159e2b37155f
Sebastian <78531972+SebastianCaron@users.noreply.github.com>
2024-04-08 17:24:49 +0200

    AJOUT DE MON GRIS !!!

--- Website/trajets.php
+++ Website/trajets.php
@@ -3,7 +3,7 @@ include 'libs/config.php';
 
 // Récupération des arrêts depuis la base de données pour les options du formulaire
 $query_arrets = "
-    SELECT Nom_Arrets
+    SELECT DISTINCT Nom_Arrets
     FROM Arrets
     GROUP BY ID_Voyages
     HAVING COUNT(*) > 4
